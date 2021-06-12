d1316daa9cb4dea70b86f2f6be6fafec6f8317e2
unknown <antoinepilote1@hotmail.com>
2021-06-12 15:10:47 -0400

    premake fixes

--- premake5.lua
+++ premake5.lua
@@ -32,7 +32,15 @@ project "Nuake"
         "%{prj.name}/src/**.h",
         "%{prj.name}/src/**.cpp",
         "%{prj.name}/src/Vendors/libmap/h/*.h",
-        "%{prj.name}/src/Vendors/libmap/c/*.c"
+        "%{prj.name}/src/Vendors/libmap/c/*.c",
+	"%{prj.name}/src/Vendors/msdfgen/**.h",
+	"%{prj.name}/src/Vendors/msdfgen/**.cpp",
+	"%{prj.name}/src/Vendors/msdf-atlas-gen/*.h",
+	"%{prj.name}/src/Vendors/msdf-atlas-gen/*.cpp",
+	"%{prj.name}/src/Vendors/wren/src/vm/*.h",
+	"%{prj.name}/src/Vendors/wren/src/vm/*.c",
+	"%{prj.name}/src/Vendors/katana-parser/*.h",
+	"%{prj.name}/src/Vendors/katana-parser/*.c"
     }
 
     includedirs
@@ -42,7 +50,11 @@ project "Nuake"
         "%{prj.name}/../Nuake/Dependencies/GLEW/include",
         "%{prj.name}/../Nuake/Dependencies/GLFW/x64/include",
         "%{prj.name}/../Nuake/Dependencies/bullet/src",
-        "%{prj.name}/../Nuake/Dependencies/assimp/include"
+        "%{prj.name}/../Nuake/Dependencies/assimp/include",
+	"%{prj.name}/../Nuake/src/Vendors/msdfgen/include",
+	"%{prj.name}/../Nuake/src/Vendors/msdfgen/freetype/include",
+	"%{prj.name}/../Nuake/src/Vendors/msdfgen",
+	"%{prj.name}/../Nuake/src/Vendors/wren/src/include"
     }
 
     filter "system:windows"
@@ -81,7 +93,11 @@ project "Editor"
         "%{prj.name}/../Nuake/dependencies/bullet/lib/release/x64",
         "%{prj.name}/../Nuake/dependencies/assimp/lib/",
         "%{prj.name}/../Nuake/dependencies/freetype-windows-binaries/release static/win64",
-        "%{prj.name}/../bin/%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}/Nuake/"
+        "%{prj.name}/../bin/%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}/Nuake/",
+	"%{prj.name}/../Nuake/src/Vendors/msdf/freetype/include",
+	"%{prj.name}/../Nuake/src/Vendors/msdfgen/freetype/win64",
+	"%{prj.name}/../Nuake/src/Vendors/msdfgen",
+	"%{prj.name}/../Nuake/src/Vendors/wren/src/include",
     }
 
     links
@@ -103,5 +119,5 @@ project "Editor"
 
     -- copy a file from the objects directory to the target directory
     postbuildcommands {
-    -- "{COPY} ../Nuake/dependencies/GLFW/lib-vc2019/glfw3.dll " .. "bin/" .. outputdir .. "/%{prj.name}/glfw3.dll"
+     "{COPY} "Nuake/dependencies/GLFW/lib-vc2019/glfw3.dll" " .. "./bin/" .. outputdir .. "/%{prj.name}/glfw3.dll"
     }
