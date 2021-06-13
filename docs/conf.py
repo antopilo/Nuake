14973e0097d65b4537b80e3a28a86877f294ce49
unknown <antoinepilote1@hotmail.com>
2021-06-13 13:48:12 -0400

    conf

--- docs/conf.py
+++ docs/conf.py
@@ -45,7 +45,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # a list of builtin themes.
 #
 html_theme = 'alabaster'
-
+html_style = '/default.css'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
