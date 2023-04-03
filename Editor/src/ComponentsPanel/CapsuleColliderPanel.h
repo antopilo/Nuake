0bf4c2cb180d97b6d5456b973271338c15665830
Antoine Pilote <antoinepilote1@hotmail.com>
2023-04-02 22:34:57 -0400

    Code cleanup for capsule panel

--- Editor/src/ComponentsPanel/CapsuleColliderPanel.h
+++ Editor/src/ComponentsPanel/CapsuleColliderPanel.h
@@ -1,35 +1,40 @@
 #pragma once
 #include "ComponentPanel.h"
+
 #include <src/Scene/Components/CapsuleColliderComponent.h>
 
-class CapsuleColliderPanel: ComponentPanel
+class CapsuleColliderPanel : ComponentPanel
 {
 public:
 	CapsuleColliderPanel() = default;
 
 	void Draw(Nuake::Entity entity) override
 	{
-		if (!entity.HasComponent<Nuake::CapsuleColliderComponent>())
-			return;
+		using namespace Nuake;
 
-		auto& component = entity.GetComponent<Nuake::CapsuleColliderComponent>();
-		BeginComponentTable(CAPSULE COLLIDER, Nuake::CapsuleColliderComponent);
+		if (!entity.HasComponent<CapsuleColliderComponent>())
+		{
+			return;
+		}
+
+		auto& [Capsule, Radius, Height, IsTrigger] = entity.GetComponent<CapsuleColliderComponent>();
+		BeginComponentTable(CAPSULE COLLIDER, CapsuleColliderComponent)
 		{
 			{
 				ImGui::Text("Radius");
 				ImGui::TableNextColumn();
-				ImGui::DragFloat("##Radius", &component.Radius);
+				ImGui::DragFloat("##Radius", &Radius);
 				ImGui::TableNextColumn();
-				ComponentTableReset(component.Radius, 0.5f)
+				ComponentTableReset(Radius, 0.5f)
 			}
 			ImGui::TableNextColumn();
 			{
 				ImGui::Text("Height");
 				ImGui::TableNextColumn();
 
-				ImGui::DragFloat("##Height", &component.Height);
+				ImGui::DragFloat("##Height", &Height);
 				ImGui::TableNextColumn();
-				ComponentTableReset(component.Height, 1.0f)
+				ComponentTableReset(Height, 1.0f)
 			}
 		}
 		EndComponentTable()
