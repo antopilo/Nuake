fa2d8d3ce90ff40aab2cc7a6c50512695e2bb27d
Antoine Pilote <antoinepilote1@hotmail.com>
2023-07-15 19:26:58 -0400

    Added Cylinder Collider panel

---
+++ Editor/src/ComponentsPanel/CylinderColliderPanel.h
@@ -0,0 +1,42 @@
+#pragma once
+#include "ComponentPanel.h"
+
+#include <src/Scene/Components/CylinderColliderComponent.h>
+
+class CylinderColliderPanel : ComponentPanel
+{
+public:
+	CylinderColliderPanel() = default;
+
+	void Draw(Nuake::Entity entity) override
+	{
+		using namespace Nuake;
+
+		if (!entity.HasComponent<CylinderColliderComponent>())
+		{
+			return;
+		}
+
+		auto& [Cylinder, Radius, Height, IsTrigger] = entity.GetComponent<CylinderColliderComponent>();
+		BeginComponentTable(CYLINDER COLLIDER, CylinderColliderComponent)
+		{
+			{
+				ImGui::Text("Radius");
+				ImGui::TableNextColumn();
+				ImGui::DragFloat("##Radius", &Radius, 0.01f, 0.001f);
+				ImGui::TableNextColumn();
+				ComponentTableReset(Radius, 0.5f)
+			}
+			ImGui::TableNextColumn();
+			{
+				ImGui::Text("Height");
+				ImGui::TableNextColumn();
+
+				ImGui::DragFloat("##Height", &Height, 0.01f, 0.001f);
+				ImGui::TableNextColumn();
+				ComponentTableReset(Height, 1.0f)
+			}
+		}
+		EndComponentTable()
+	}
+};
