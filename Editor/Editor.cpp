752f9ac8f87fbb8eac7ae62e9098c4291f69b1ac
Antoine Pilote <antoinepilote1@hotmail.com>
2023-07-16 14:12:01 -0400

    Compilation fix

--- Editor/Editor.cpp
+++ Editor/Editor.cpp
@@ -196,14 +196,14 @@ int main(int argc, char* argv[])
 
                     float nearZ = 0.1f;
                     float farZ = 1000.0f;
-                    gizmoDrawer.GetLineShader().SetUniformTex("u_Depth", depthTexture.get(), 3);
-                    gizmoDrawer.GetLineShader().SetUniformVec2("u_Resolution", depthTexture->GetSize());
+                    //gizmoDrawer.GetLineShader().SetUniformTex("u_Depth", depthTexture.get(), 3);
+                    //gizmoDrawer.GetLineShader().SetUniformVec2("u_Resolution", depthTexture->GetSize());
                     glEnable(GL_LINE_SMOOTH);
                     glEnable(GL_BLEND);
                     glDepthRange(0.1f, 1000.0f);
                     if (editor.ShouldDrawAxis())
                     {
-                        gizmoDrawer.DrawAxis(currentScene);
+                        //gizmoDrawer.DrawAxis(currentScene);
                     }
 
                     if (editor.ShouldDrawCollision())
