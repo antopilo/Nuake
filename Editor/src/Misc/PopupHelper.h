63b5958a640cc924d02dbeb6bb3dc8d021e92f2c
Émeryc <emeryc.primeau@gmail.com>
2023-07-21 21:07:11 -0400

    NK-102 - PR Comments

--- Editor/src/Misc/PopupHelper.h
+++ Editor/src/Misc/PopupHelper.h
@@ -5,5 +5,5 @@ class PopupHelper
 {
 public:
     static void Confirmation(const std::string& id);
-    static bool DefineDialog(std::string id, std::string text);
+    static bool DefineDialog(std::string& id, std::string& text);
 };
