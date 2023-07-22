04d6241b63f72af7d196e0e14d72b0450f70a3ad
Émeryc <emeryc.primeau@gmail.com>
2023-07-21 21:03:35 -0400

    NK-102 - Generic Confirmation Popup

---
+++ Editor/src/Misc/PopupHelper.h
@@ -0,0 +1,9 @@
+﻿#pragma once
+#include <string>
+
+class PopupHelper
+{
+public:
+    static void Confirmation(const std::string& id);
+    static bool DefineDialog(std::string id, std::string text);
+};
