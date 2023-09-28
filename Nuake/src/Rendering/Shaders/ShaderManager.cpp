324e881a8d5d52d45321ad585bb9e8d6d9e5c910
MissLavender-LQ <MissLav@protonmail.com>
2023-09-28 04:27:51 +0100

    embed fix by @antopilo

--- Nuake/src/Rendering/Shaders/ShaderManager.cpp
+++ Nuake/src/Rendering/Shaders/ShaderManager.cpp
@@ -4,7 +4,7 @@
 #include "src/Resource/StaticResources.h"
 
 #define LoadEmbeddedShader(file) \
-m_Shaders[file##_path] = CreateScope<Shader>(file##_path, std::string(file, file + file##_len));
+m_Shaders[file##_path] = CreateScope<Shader>(file##_path, std::string(reinterpret_cast<const char*>(file), reinterpret_cast<const char*>(file) + file##_len));
 
 namespace Nuake
 {
@@ -14,33 +14,33 @@ namespace Nuake
 	{
 		using namespace StaticResources;
 
-		// LoadEmbeddedShader(Resources_Shaders_atmospheric_sky_shader);
-		// LoadEmbeddedShader(Resources_Shaders_barrel_distortion_shader);
-		// LoadEmbeddedShader(Resources_Shaders_bloom_shader);
-		// LoadEmbeddedShader(Resources_Shaders_blur_shader);
-		// LoadEmbeddedShader(Resources_Shaders_BRD_shader);
-		// LoadEmbeddedShader(Resources_Shaders_combine_shader);
-		// LoadEmbeddedShader(Resources_Shaders_copy_shader);
-		// LoadEmbeddedShader(Resources_Shaders_deferred_shader);
-		// LoadEmbeddedShader(Resources_Shaders_depth_aware_blur_shader);
-		// LoadEmbeddedShader(Resources_Shaders_dither_shader);
-		// LoadEmbeddedShader(Resources_Shaders_dof_shader);
-		// LoadEmbeddedShader(Resources_Shaders_flat_shader);
-		// LoadEmbeddedShader(Resources_Shaders_gbuffer_shader);
-		// LoadEmbeddedShader(Resources_Shaders_gbuffer_skinned_shader);
-		// LoadEmbeddedShader(Resources_Shaders_gizmo_shader);
-		// LoadEmbeddedShader(Resources_Shaders_line_shader);
-		// LoadEmbeddedShader(Resources_Shaders_pbr_shader);
-		// LoadEmbeddedShader(Resources_Shaders_sdf_text_shader);
-		// LoadEmbeddedShader(Resources_Shaders_shadowMap_shader);
-		// LoadEmbeddedShader(Resources_Shaders_shadowMap_skinned_shader);
-		// LoadEmbeddedShader(Resources_Shaders_skybox_shader);
-		// LoadEmbeddedShader(Resources_Shaders_ssao_shader);
-		// LoadEmbeddedShader(Resources_Shaders_ssr_shader);
-		// LoadEmbeddedShader(Resources_Shaders_tonemap_shader);
-		// LoadEmbeddedShader(Resources_Shaders_ui_shader);
-		// LoadEmbeddedShader(Resources_Shaders_vignette_shader);
-		// LoadEmbeddedShader(Resources_Shaders_volumetric_shader);
+		LoadEmbeddedShader(Resources_Shaders_atmospheric_sky_shader);
+		LoadEmbeddedShader(Resources_Shaders_barrel_distortion_shader);
+		LoadEmbeddedShader(Resources_Shaders_bloom_shader);
+		LoadEmbeddedShader(Resources_Shaders_blur_shader);	
+		LoadEmbeddedShader(Resources_Shaders_BRD_shader);
+		LoadEmbeddedShader(Resources_Shaders_combine_shader);
+		LoadEmbeddedShader(Resources_Shaders_copy_shader);
+		LoadEmbeddedShader(Resources_Shaders_deferred_shader);
+		LoadEmbeddedShader(Resources_Shaders_depth_aware_blur_shader);
+		LoadEmbeddedShader(Resources_Shaders_dither_shader);
+		LoadEmbeddedShader(Resources_Shaders_dof_shader);
+		LoadEmbeddedShader(Resources_Shaders_flat_shader);
+		LoadEmbeddedShader(Resources_Shaders_gbuffer_shader);
+		LoadEmbeddedShader(Resources_Shaders_gbuffer_skinned_shader);
+		LoadEmbeddedShader(Resources_Shaders_gizmo_shader);
+		LoadEmbeddedShader(Resources_Shaders_line_shader);
+		LoadEmbeddedShader(Resources_Shaders_pbr_shader);
+		LoadEmbeddedShader(Resources_Shaders_sdf_text_shader);
+		LoadEmbeddedShader(Resources_Shaders_shadowMap_shader);
+		LoadEmbeddedShader(Resources_Shaders_shadowMap_skinned_shader);
+		LoadEmbeddedShader(Resources_Shaders_skybox_shader);
+		LoadEmbeddedShader(Resources_Shaders_ssao_shader);
+		LoadEmbeddedShader(Resources_Shaders_ssr_shader);
+		LoadEmbeddedShader(Resources_Shaders_tonemap_shader);
+		LoadEmbeddedShader(Resources_Shaders_ui_shader);
+		LoadEmbeddedShader(Resources_Shaders_vignette_shader);
+		LoadEmbeddedShader(Resources_Shaders_volumetric_shader);
 	}
 
 	Shader* ShaderManager::GetShader(const std::string& path)
--- Nuake/src/Scripting/ScriptingEngine.cpp
+++ Nuake/src/Scripting/ScriptingEngine.cpp
@@ -66,7 +66,7 @@ namespace Nuake {
         }
     }
 
-#define GET_STATIC_RESOURCE_SCRIPT_SRC(name) std::string(name, name + name##_len)
+#define GET_STATIC_RESOURCE_SCRIPT_SRC(name) std::string(reinterpret_cast<const char*>(name), reinterpret_cast<const char*>(name) + name##_len)
 
     std::map<std::string, std::string> _ModulesSRC =
     {
@@ -95,15 +95,15 @@ namespace Nuake {
             size_t prefixPosition = sname.find(NuakeModulePrefix);
             sname.erase(prefixPosition, NuakeModulePrefix.length());
             path = "Resources/Scripts/"+std::string(sname);
-            isAbsolute = true;
-            // if (_ModulesSRC.find(sname) != _ModulesSRC.end())
-            // {
-            //     fileContent = _ModulesSRC[sname];
-            // }
-            // else
-            // {
-            //     Logger::Log("Internal script module not found: " + sname + ". \n Did you forget to register it?", "scripting engine", CRITICAL);
-            // }
+            
+            if (_ModulesSRC.find(sname) != _ModulesSRC.end())
+            {
+                fileContent = _ModulesSRC[sname];
+            }
+            else
+            {
+                Logger::Log("Internal script module not found: " + sname + ". \n Did you forget to register it?", "scripting engine", CRITICAL);
+            }
         }
         else
         {
