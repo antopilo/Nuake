7b1d930b9f2b509ff70c8ffe81d9fd7e9e816f8c
Antoine Pilote <antoinepilote1@hotmail.com>
2024-07-24 21:14:23 -0400

    Moved nav mesh generation button onto navmeshVolume component.

--- Editor/src/ComponentsPanel/NavMeshVolumePanel.h
+++ Editor/src/ComponentsPanel/NavMeshVolumePanel.h
@@ -49,13 +49,54 @@ public:
             }
             ImGui::TableNextColumn();
             {
+                CompononentPropertyName("Only include map");
+                ImGui::TableNextColumn();
+
+                Vector3 volumeSize = component.VolumeSize;
+
+                UI::CheckBox("##onlyIncludeMapGeo", component.OnlyIncludeMapGeometry);
+
+                ImGui::TableNextColumn();
+
+                ImGui::PushStyleColor(ImGuiCol_Button, ImVec4(1, 1, 1, 0));
+                std::string resetLabel = std::string(ICON_FA_UNDO) + "##ResetOnlyIncludeGeo";
+                if (ImGui::Button(resetLabel.c_str()))
+                {
+                    component.OnlyIncludeMapGeometry = true;
+                }
+
+                ImGui::PopStyleColor();
+            }
+            ImGui::TableNextColumn();
+            {
                 CompononentPropertyName("Generate");
 
                 ImGui::TableNextColumn();
 
                 if (UI::SecondaryButton("Generate"))
                 {
+                    if (component.OnlyIncludeMapGeometry)
+                    {
+                        auto view = Engine::GetCurrentScene()->m_Registry.view<TransformComponent, QuakeMapComponent>();
+                        for (auto e : view)
+                        {
+                            auto [transform, map] = view.get<TransformComponent, QuakeMapComponent>(e);
 
+                            for (auto& b : map.m_Brushes)
+                            {
+                                if (b.HasComponent<ModelComponent>())
+                                {
+                                    TransformComponent& transformComponent = b.GetComponent<TransformComponent>();
+                                    for (auto& mesh : b.GetComponent<ModelComponent>().ModelResource->GetMeshes())
+                                    {
+                                        Nuake::NavManager::Get().PushMesh(mesh, transformComponent.GetGlobalTransform());
+                                    }
+                                }
+                            }
+
+                            NavManager::Get().BuildNavMesh();
+                        }
+                    }
                 }
 
                 ImGui::TableNextColumn();
--- Nuake/src/Scene/Components/NavMeshVolumeComponent.h
+++ Nuake/src/Scene/Components/NavMeshVolumeComponent.h
@@ -7,11 +7,13 @@ namespace Nuake
 	struct NavMeshVolumeComponent
 	{
 		Vector3 VolumeSize = { 1.0f, 1.0f, 1.0f };
+		bool OnlyIncludeMapGeometry = true;
 
 		json Serialize()
 		{
 			BEGIN_SERIALIZE();
 			SERIALIZE_VEC3(VolumeSize);
+			SERIALIZE_VAL(OnlyIncludeMapGeometry);
 			END_SERIALIZE();
 		}
 
@@ -20,6 +22,11 @@ namespace Nuake
 			if (j.contains("VolumeSize"));
 			{
 				DESERIALIZE_VEC3(j["VolumeSize"], VolumeSize);
+			}
+
+			if (j.contains("OnlyIncludeMapGeometry"))
+			{
+				DESERIALIZE_VAL(OnlyIncludeMapGeometry);
 			}
 
 			return true;
