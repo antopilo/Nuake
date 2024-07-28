d4f783a1dc7b1ebf98ef93d15e8a1ee5a519284b
Antoine Pilote <antoinepilote1@hotmail.com>
2024-07-28 10:59:10 -0400

    Added recast premake5

---
+++ Nuake/dependencies/recastnavigation_p5.lua
@@ -0,0 +1,76 @@
+project "DebugUtils"
+	language "C++"
+	kind "StaticLib"
+	includedirs { 
+		"recastnavigation/DebugUtils/Include",
+		"recastnavigation/Detour/Include",
+		"recastnavigation/DetourTileCache/Include",
+		"recastnavigation/Recast/Include"
+	}
+	files {
+		"recastnavigation/DebugUtils/Include/*.h",
+		"recastnavigation/DebugUtils/Source/*.cpp"
+	}
+
+project "DetourCrowd"
+	language "C++"
+	kind "StaticLib"
+	includedirs {
+		"recastnavigation/DetourCrowd/Include",
+		"recastnavigation/Detour/Include",
+		"recastnavigation/Recast/Include"
+	}
+	files {
+		"recastnavigation/DetourCrowd/Include/*.h",
+		"recastnavigation/DetourCrowd/Source/*.cpp"
+	}
+
+project "DetourTileCache"
+	language "C++"
+	kind "StaticLib"
+	includedirs {
+		"recastnavigation/DetourTileCache/Include",
+		"recastnavigation/Detour/Include",
+		"recastnavigation/Recast/Include"
+	}
+	files {
+		"recastnavigation/DetourTileCache/Include/*.h",
+		"recastnavigation/DetourTileCache/Source/*.cpp"
+	}
+
+project "Recast"
+	language "C++"
+	kind "StaticLib"
+	includedirs { 
+		"recastnavigation/Recast/Include" 
+	}
+	files { 
+		"recastnavigation/Recast/Include/*.h",
+		"recastnavigation/Recast/Source/*.cpp" 
+	}
+
+project "Detour"
+	language "C++"
+	kind "StaticLib"
+	staticruntime "on"
+
+	includedirs { 
+		"recastnavigation/Detour/Include" 
+	}
+	files { 
+		"recastnavigation/Detour/Include/*.h", 
+		"recastnavigation/Detour/Source/*.cpp" 
+	}
+
+	filter "configurations:Debug"
+		defines {"DEBUG"}
+		symbols "On"
+		postbuildcommands {}
+		flags {"staticruntime"} -- this is what worked for me
+
+	-- linux library cflags and libs
+	filter {"system:linux", "toolset:gcc"}
+		buildoptions {
+			"-Wno-error=class-memaccess",
+			"-Wno-error=maybe-uninitialized"
+		}
